5d83d44f89e6f17db68851d8f3623d31aa1f401c
Abdurahman <haydarabdurahman@gmail.com>
2023-01-21 19:46:08 +0200

    initial  fitrah app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-1xl font-bold underline">
+          Initial Fitrah App UI
+         </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
