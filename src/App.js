00076d34920a8c57f23ae4041dbde31539bad36d
abdulkhaliq-cyber <akh.haydar5@gmail.com>
2023-02-23 18:51:50 +0200

    hopefullly this is good

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-import recipientList from './recipientList';
+import Table from './components/Table';
+
+
+
 
 function App() {
+
+  const [search, setSearch] = useState('');
+  console.log(search)
+
+  const [dataTable, setDataTable] = useState([]);
+  console.log(dataTable);
+
+useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/users')
+      .then(res => setDataTable(res.data))
+      .catch(err => console.log(err))
+  }, []);
+
+  
+
+  const column = [
+    {heading: 'Name', value: 'name'},
+    {heading: 'Email', value: 'email'},
+    {heading: 'Phone', value: 'phone'},
+    {heading: 'City', value: 'address.city'},
+  ]
+
+  const search1 = (data) => {
+    return data.filter(item=> item.name.toLowerCase().includes(search))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-1xl font-bold underline">
-          Initial Fitrah App UI
-         </h1>
-      </header>
+      <div>
+      
+        <input 
+        type="text"
+        placeholder='Search Recipient'
+        className='search'
+        onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      
+      <div>
+        <h1>Dynamic Table</h1>
+        <Table data={search1(dataTable)} column={column}/>
+      </div>
+      
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,37 @@
+import './Table.css'
+
+const Table = ({data, column}) => {
+    return (
+        <div>
+          <table>
+            <thead>
+                <tr>
+                    {column.map((item, index )=> <TableHeadItem item={item}/>)}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, index) => <TableRow item={item} column={column}/>)}
+            </tbody>
+          </table>
+        </div>
+
+    )
+}
+
+const TableHeadItem = ({item}) => <th>{item.heading}</th>
+const TableRow = ({item, column}) => (
+    <tr key={item.id}>
+        {column.map((columnItem, index) => {
+
+            if(columnItem.value.includes('.')) {
+                const itemSplit = columnItem.value.split('.')
+                return <td>{item[itemSplit[0]][itemSplit[1]]}</td>
+            }
+
+
+            return <td>{item[`${columnItem.value}`]}</td>
+        })}
+    </tr>
+)
+
+export default Table;
